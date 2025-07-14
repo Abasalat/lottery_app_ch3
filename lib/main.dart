f5dda6ad7e0a197f3afe5da81f5f547d25f5a2c7
Abasalat <abasalat.bscsf21@iba-suk.edu.pk>
2025-07-14 13:25:01 +0500

    create the lottery app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(Myapp());
@@ -8,33 +9,107 @@ class Myapp extends StatefulWidget {
   const Myapp({super.key});
 
   @override
-  State<Myapp> createState() => _MyWidgetState();
+  State<Myapp> createState() => _MyappState();
 }
 
-class _MyWidgetState extends State<Myapp> {
+class _MyappState extends State<Myapp> {
+  Random random = Random();
   int x = 0;
+  int y = 3;
 
   @override
   Widget build(BuildContext context) {
-    print('rebuild');
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Lottery app")),
-        body: SafeArea(
-          child: Center(
-            child: Text(x.toString(), style: TextStyle(fontSize: 50)),
-          ),
+        appBar: AppBar(title: Text('Lottery App')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(child: Text('Lottery winning number is $y')),
+            SizedBox(height: 15),
+            Container(
+              height: x == y ? 600 : 300,
+              width: 300,
+              decoration: BoxDecoration(
+                color: x == y ? Colors.lightGreen : Colors.grey,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: x == y
+                    ? Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(Icons.done_all, color: Colors.green, size: 35),
+                          SizedBox(height: 15),
+                          Text(
+                            'Congratulations you won the lottery ',
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      )
+                    : Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(Icons.error, color: Colors.red, size: 35),
+                          SizedBox(height: 15),
+                          Text(
+                            'Better luck next time your number is $x\n try again',
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+              ),
+            ),
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            x++;
-            setState(() {});
-            print('tab');
+            x = random.nextInt(5);
             print(x.toString());
+            setState(() {});
           },
-          child: Icon(Icons.add),
+          child: Icon(Icons.refresh),
         ),
       ),
     );
   }
 }
+
+// class Myapp extends StatefulWidget {
+//   const Myapp({super.key});
+
+//   @override
+//   State<Myapp> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<Myapp> {
+//   int x = 0;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     print('rebuild');
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(title: Text("Lottery app")),
+//         body: SafeArea(
+//           child: Center(
+//             child: Text(x.toString(), style: TextStyle(fontSize: 50)),
+//           ),
+//         ),
+//         floatingActionButton: FloatingActionButton(
+//           onPressed: () {
+//             x++;
+//             setState(() {});
+//             print('tab');
+//             print(x.toString());
+//           },
+//           child: Icon(Icons.add),
+//         ),
+//       ),
+//     );
+//   }
+// }
