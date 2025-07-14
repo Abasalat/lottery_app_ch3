a997c7afda00da2549886f42e04c65d5fff2f955
Abasalat <abasalat.bscsf21@iba-suk.edu.pk>
2025-07-14 14:28:01 +0500

    Revert "create the lottery app"
    This reverts commit f5dda6ad7e0a197f3afe5da81f5f547d25f5a2c7.
    
    qa

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
 void main() {
   runApp(Myapp());
@@ -9,107 +8,33 @@ class Myapp extends StatefulWidget {
   const Myapp({super.key});
 
   @override
-  State<Myapp> createState() => _MyappState();
+  State<Myapp> createState() => _MyWidgetState();
 }
 
-class _MyappState extends State<Myapp> {
-  Random random = Random();
+class _MyWidgetState extends State<Myapp> {
   int x = 0;
-  int y = 3;
 
   @override
   Widget build(BuildContext context) {
+    print('rebuild');
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Lottery App')),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(child: Text('Lottery winning number is $y')),
-            SizedBox(height: 15),
-            Container(
-              height: x == y ? 600 : 300,
-              width: 300,
-              decoration: BoxDecoration(
-                color: x == y ? Colors.lightGreen : Colors.grey,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: x == y
-                    ? Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Icon(Icons.done_all, color: Colors.green, size: 35),
-                          SizedBox(height: 15),
-                          Text(
-                            'Congratulations you won the lottery ',
-                            textAlign: TextAlign.center,
-                          ),
-                        ],
-                      )
-                    : Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Icon(Icons.error, color: Colors.red, size: 35),
-                          SizedBox(height: 15),
-                          Text(
-                            'Better luck next time your number is $x\n try again',
-                            textAlign: TextAlign.center,
-                          ),
-                        ],
-                      ),
-              ),
-            ),
-          ],
+        appBar: AppBar(title: Text("Lottery app")),
+        body: SafeArea(
+          child: Center(
+            child: Text(x.toString(), style: TextStyle(fontSize: 50)),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            x = random.nextInt(5);
-            print(x.toString());
+            x++;
             setState(() {});
+            print('tab');
+            print(x.toString());
           },
-          child: Icon(Icons.refresh),
+          child: Icon(Icons.add),
         ),
       ),
     );
   }
 }
-
-// class Myapp extends StatefulWidget {
-//   const Myapp({super.key});
-
-//   @override
-//   State<Myapp> createState() => _MyWidgetState();
-// }
-
-// class _MyWidgetState extends State<Myapp> {
-//   int x = 0;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     print('rebuild');
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(title: Text("Lottery app")),
-//         body: SafeArea(
-//           child: Center(
-//             child: Text(x.toString(), style: TextStyle(fontSize: 50)),
-//           ),
-//         ),
-//         floatingActionButton: FloatingActionButton(
-//           onPressed: () {
-//             x++;
-//             setState(() {});
-//             print('tab');
-//             print(x.toString());
-//           },
-//           child: Icon(Icons.add),
-//         ),
-//       ),
-//     );
-//   }
-// }
