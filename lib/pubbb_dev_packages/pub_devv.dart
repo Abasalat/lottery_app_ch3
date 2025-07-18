c8634f52350ca386ad3a7ac826104b61e38eca33
Abasalat <abasalat.bscsf21@iba-suk.edu.pk>
2025-07-18 11:02:35 +0500

    Add readmore  package

--- lib/pubbb_dev_packages/pub_devv.dart
+++ lib/pubbb_dev_packages/pub_devv.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+//import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:readmore/readmore.dart';
 
 class PubDevv extends StatelessWidget {
   const PubDevv({super.key});
@@ -12,11 +13,23 @@ class PubDevv extends StatelessWidget {
         child: Column(
           children: [
             Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Icon(FontAwesomeIcons.searchengin),
+              padding: const EdgeInsets.all(10),
+              child: ReadMoreText(
+                'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsu',
+                textAlign: TextAlign.center,
+                trimLines: 2,
+                trimMode: TrimMode.Line,
+                // trimLength: 50,
+                trimExpandedText: "Read Less",
+                trimCollapsedText: "Read All",
+                moreStyle: TextStyle(color: Colors.deepOrange),
+                style: TextStyle(color: Colors.cyanAccent),
+                colorClickableText: Colors.amber,
+              ),
             ),
-            Icon(FontAwesomeIcons.arrowDown),
-            Icon(FontAwesomeIcons.addressCard),
+            // Icon(FontAwesomeIcons.searchengin),
+            // Icon(FontAwesomeIcons.arrowDown),
+            // Icon(FontAwesomeIcons.addressCard),
           ],
         ),
       ),
